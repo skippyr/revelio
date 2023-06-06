44911b01546ea3b2662eb221ece1c7fda27795ef
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-06 08:36:58 -0300

    feat: add permissions

--- src/file_system.rs
+++ src/file_system.rs
@@ -87,12 +87,155 @@ impl Display for EntryKind {
 	}
 }
 
+pub struct Permissions {
+	mode: u32
+}
+
+const UNIX_OWNER_READING_PERMISSIONS_BIT: u32 = 0o400;
+const UNIX_OWNER_WRITING_PERMISSIONS_BIT: u32 = 0o200;
+const UNIX_OWNER_EXECUTION_PERMISSIONS_BIT: u32 = 0o100;
+const UNIX_GROUP_READING_PERMISSIONS_BIT: u32 = 0o40;
+const UNIX_GROUP_WRITING_PERMISSIONS_BIT: u32 = 0o20;
+const UNIX_GROUP_EXECUTION_PERMISSIONS_BIT: u32 = 0o10;
+const UNIX_OTHERS_READING_PERMISSIONS_BIT: u32 = 0o4;
+const UNIX_OTHERS_WRITING_PERMISSIONS_BIT: u32 = 0o2;
+const UNIX_OTHERS_EXECUTION_PERMISSIONS_BIT: u32 = 0o1;
+
+impl Permissions {
+	pub fn from_mode(mode: u32) -> Self {
+		Self {mode}
+	}
+
+	fn does_owner_can_read(&self) -> bool {
+		self.mode & UNIX_OWNER_READING_PERMISSIONS_BIT != 0
+	}
+
+	fn does_owner_can_write(&self) -> bool {
+		self.mode & UNIX_OWNER_WRITING_PERMISSIONS_BIT != 0
+	}
+
+	fn does_owner_can_execute(&self) -> bool {
+		self.mode & UNIX_OWNER_EXECUTION_PERMISSIONS_BIT != 0
+	}
+
+	fn does_group_can_read(&self) -> bool {
+		self.mode & UNIX_GROUP_READING_PERMISSIONS_BIT != 0
+	}
+
+	fn does_group_can_write(&self) -> bool {
+		self.mode & UNIX_GROUP_WRITING_PERMISSIONS_BIT != 0
+	}
+
+	fn does_group_can_execute(&self) -> bool {
+		self.mode & UNIX_GROUP_EXECUTION_PERMISSIONS_BIT != 0
+	}
+
+	fn does_others_can_read(&self) -> bool {
+		self.mode & UNIX_OTHERS_READING_PERMISSIONS_BIT != 0
+	}
+
+	fn does_others_can_write(&self) -> bool {
+		self.mode & UNIX_OTHERS_WRITING_PERMISSIONS_BIT != 0
+	}
+
+	fn does_others_can_execute(&self) -> bool {
+		self.mode & UNIX_OTHERS_EXECUTION_PERMISSIONS_BIT != 0
+	}
+
+	pub fn as_bits_sum(&self) -> u32 {
+		let mut sum: u32 = 0;
+		if self.does_owner_can_read() {
+			sum += UNIX_OWNER_READING_PERMISSIONS_BIT;
+		}
+		if self.does_owner_can_write() {
+			sum += UNIX_OWNER_WRITING_PERMISSIONS_BIT;
+		}
+		if self.does_owner_can_execute() {
+			sum += UNIX_OWNER_EXECUTION_PERMISSIONS_BIT;
+		}
+		if self.does_group_can_read() {
+			sum += UNIX_GROUP_READING_PERMISSIONS_BIT;
+		}
+		if self.does_group_can_write() {
+			sum += UNIX_GROUP_WRITING_PERMISSIONS_BIT;
+		}
+		if self.does_group_can_execute() {
+			sum += UNIX_GROUP_EXECUTION_PERMISSIONS_BIT;
+		}
+		if self.does_others_can_read() {
+			sum += UNIX_OTHERS_READING_PERMISSIONS_BIT;
+		}
+		if self.does_others_can_write() {
+			sum += UNIX_OTHERS_WRITING_PERMISSIONS_BIT;
+		}
+		if self.does_others_can_execute() {
+			sum += UNIX_OTHERS_EXECUTION_PERMISSIONS_BIT;
+		}
+		sum
+	}
+
+	pub fn as_string(&self) -> String {
+		const READING_CHARACTER: char = 'r';
+		const WRITING_CHARACTER: char = 'w';
+		const EXECUTION_CHARACTER: char = 'x';
+		const NONE_CHARACTER: char = '-';
+		let mut string: String = String::new();
+		if self.does_owner_can_read() {
+			string.push(READING_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_owner_can_write() {
+			string.push(WRITING_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_owner_can_execute() {
+			string.push(EXECUTION_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_group_can_read() {
+			string.push(READING_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_group_can_write() {
+			string.push(WRITING_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_group_can_execute() {
+			string.push(EXECUTION_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_others_can_read() {
+			string.push(READING_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_others_can_write() {
+			string.push(WRITING_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		if self.does_others_can_execute() {
+			string.push(EXECUTION_CHARACTER);
+		} else {
+			string.push(NONE_CHARACTER);
+		}
+		string
+	}
+}
+
 struct Entry {
 	name: String,
 	kind: EntryKind,
 	size: u64,
 	owner: Option<String>,
-	symlink_path: Option<String>
+	symlink_path: Option<String>,
+	permissions: Permissions
 }
 
 fn convert_size_to_human_readable_format(size: u64) -> String {
@@ -159,11 +302,13 @@ pub fn reveal_directory(path: &PathBuf) {
 					kind: EntryKind::Broken,
 					size: 0,
 					owner: None,
-					symlink_path
+					symlink_path,
+					permissions: Permissions::from_mode(0)
 				});
 				continue;
 			}
 		};
+		let permissions = Permissions::from_mode(metadata.mode());
 		let file_type = metadata.file_type();
 		let kind = EntryKind::from_file_type(&file_type);
 		let size = if let EntryKind::File = kind {
@@ -186,18 +331,22 @@ pub fn reveal_directory(path: &PathBuf) {
 			kind,
 			size,
 			owner,
-			symlink_path
+			symlink_path,
+			permissions
 		});
 	}
 	entries.sort_by_key(|entry| {entry.name.clone()});
+	println!("Owner       Size     Permissions    Type       Name");
 	for entry in entries {
 		println!(
-			"{:<10}  {:<7}  {:<9}  {}{}",
+			"{:<10}  {:<7}  {} {:o}  {:<9}  {}{}",
 			match entry.owner {
 				Some(owner) => {owner}
 				None => String::from("-")
 			},
 			convert_size_to_human_readable_format(entry.size),
+			entry.permissions.as_string(),
+			entry.permissions.as_bits_sum(),
 			format!("{}", entry.kind),
 			entry.name,
 			match entry.symlink_path {
