3dc8f4c93b794290a69d2c35627172de0f5bbc2d
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-06 07:41:37 -0300

    feat: add remaining of function to get entry metadata

--- src/bin/reveal.rs
+++ src/bin/reveal.rs
@@ -4,14 +4,20 @@ use std::{
 	path::PathBuf,
 	fs::{
 		File,
-		Metadata,
-		read_dir
+		FileType,
+		read_dir,
+		read_link
 	},
 	io::{
 		BufReader,
 		BufRead
+	},
+	os::unix::fs::{
+		FileTypeExt,
+		MetadataExt
 	}
 };
+use users::get_user_by_uid;
 
 fn throw_error(description: &str, suggestion: &str) -> ! {
 	eprintln!("Error");
@@ -60,10 +66,32 @@ enum EntryKind {
 	Broken
 }
 
+impl EntryKind {
+	fn from_file_type(file_type: &FileType) -> Self {
+		if file_type.is_dir() {
+			Self::Directory
+		} else if file_type.is_file() {
+			Self::File
+		} else if file_type.is_socket() {
+			Self::Socket
+		} else if file_type.is_char_device() {
+			Self::Character
+		} else if file_type.is_block_device() {
+			Self::Block
+		} else if file_type.is_fifo() {
+			Self::Fifo
+		} else {
+			Self::Broken
+		}
+	}
+}
+
 struct Entry {
 	name: String,
 	kind: EntryKind,
-	size: u64
+	size: u64,
+	owner: Option<String>,
+	symlink_path: Option<String>
 }
 
 fn reveal_directory(path: &PathBuf) {
@@ -85,28 +113,46 @@ fn reveal_directory(path: &PathBuf) {
 			Some(name) => {String::from(name)}
 			None => {format!("{}", entry.path().display())}
 		};
+		let symlink_path = match read_link(entry.path()) {
+			Ok(symlink_path) => {Some(format!("{}", symlink_path.display()))}
+			Err(..) => {None}
+		};
 		let metadata = match entry.path().metadata() {
 			Ok(metadata) => {metadata}
 			Err(..) => {
 				entries.push(Entry {
 					name,
 					kind: EntryKind::Broken,
-					size: 0
+					size: 0,
+					owner: None,
+					symlink_path
 				});
 				continue;
 			}
 		};
 		let file_type = metadata.file_type();
-		let kind = if file_type.is_dir() {
-			EntryKind::Directory
+		let kind = EntryKind::from_file_type(&file_type);
+		let size = if let EntryKind::File = kind {
+			metadata.size()
 		} else {
-			EntryKind::File
+			0
 		};
-		let size = 0;
+		let owner_uid = metadata.uid();
+		let owner = match get_user_by_uid(owner_uid) {
+			Some(user) => {
+				match user.name().to_str() {
+					Some(owner) => {Some(String::from(owner))}
+					None => {None}
+				}
+			}
+			None => {None}
+		};
 		entries.push(Entry {
 			name,
 			kind,
-			size
+			size,
+			owner,
+			symlink_path
 		});
 	}
 }
