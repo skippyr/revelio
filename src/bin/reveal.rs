6d2cf642f4cb6f68a362e7d1850c82f75b19dcb9
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-05 23:01:38 -0300

    feat: add function to reveal file

--- src/bin/reveal.rs
+++ src/bin/reveal.rs
@@ -1,18 +1,56 @@
 use std::{
 	process::exit,
 	env::args,
-	path::PathBuf
+	path::PathBuf,
+	fs::File,
+	io::{
+		BufReader,
+		BufRead
+	}
 };
 
 fn throw_error(description: &str, suggestion: &str) -> ! {
 	eprintln!("Error");
-	eprintln!("\tDescription:\n\t\t{}", description);
-	eprintln!("\tSuggestion:\n\t\t{}", suggestion);
+	eprintln!("\tDescription\n\t\t{}", description);
+	eprintln!("\tSuggestion\n\t\t{}", suggestion);
 	exit(1);
+}
+
+fn print_help_instructions() -> ! {
+	println!("Reveal - Help Instructions");
+	println!("\tStarting Point");
+	println!("\t\tA program to reveal directory entries and file contents.");
+	println!("\tSyntax");
+	println!("\t\treveal [flags] <path>");
+	println!("\tFlags");
+	println!("\t\t--help: print these help instructions.");
+	exit(0);
+}
+
+fn reveal_file(path: &PathBuf) {
+	let file = File::open(path).unwrap_or_else(|_| {
+		throw_error(
+			"Could not open file.",
+			"Ensure that you have enough permissions to read it."
+		);
+	});
+	let reader = BufReader::new(file);
+	for line in reader.lines() {
+		let line = line.unwrap_or_else(|_| {
+			throw_error(
+				"Could not read lines of file.",
+				"Ensure that the file is not a binary."
+			);
+		});
+		eprintln!("{}", line);
+	}
 }
 
 fn main() {
 	let arguments = args().collect::<Vec<String>>();
+	if arguments.contains(&String::from("--help")) {
+		print_help_instructions();
+	}
 	if arguments.len() < 2 {
 		throw_error(
 			"Not enough arguments.",
@@ -23,7 +61,7 @@ fn main() {
 		throw_error(
 			"The given path does not exists.",
 			"Ensure that you did not mispell it."
-		)
+		);
 	});
 	let metadata = path.metadata().unwrap_or_else(|_| {
 		throw_error(
@@ -32,7 +70,7 @@ fn main() {
 		);
 	});
 	if metadata.is_file() {
-		eprintln!("Revealing A File");
+		reveal_file(&path);
 	} else if metadata.is_dir() {
 		eprintln!("Revealing A Directory");
 	} else {
