7e706698ed3cee28bb0ac32dc454adff60a8ad49
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-15 18:53:49 -0300

    feat: add permissions and entry name

--- reveal.go
+++ reveal.go
@@ -4,6 +4,7 @@ import (
 	"github.com/skippyr/graffiti"
 	"os"
 	"path/filepath"
+	"fmt"
 )
 
 func throwError(description string, suggestion string) {
@@ -15,8 +16,48 @@ func throwError(description string, suggestion string) {
 	graffiti.Eprintln("    @BSuggestion")
 	graffiti.Eprintln("        %s", suggestion)
 	graffiti.Eprintln("")
-	graffiti.Eprintln("Program exited with exit code @F{cyan}%d@r.", exitCode)
+	graffiti.Eprintln("Program exited with exit code %d.", exitCode)
 	os.Exit(exitCode)
+}
+
+func colorizeMode(mode *string) string {
+	var coloredMode string
+	for characterIterator, character := range *mode {
+		if character == '-' {
+			coloredMode += string(character)
+		} else if characterIterator == 0 {
+			coloredMode += fmt.Sprintf("@F{cyan}%c@r", character)
+		} else if characterIterator == 1 || characterIterator == 4 || characterIterator == 7 {
+			coloredMode += fmt.Sprintf("@F{green}%c@r", character)
+		} else if characterIterator == 2 || characterIterator == 5 || characterIterator == 8 {
+			coloredMode += fmt.Sprintf("@F{yellow}%c@r", character)
+		} else {
+			coloredMode += fmt.Sprintf("@F{red}%c@r", character)
+		}
+	}
+	return coloredMode
+}
+
+func revealDirectory(path *string) {
+	entries, err := os.ReadDir(*path)
+	if err != nil {
+		throwError(
+			"Could not reveal directory.",
+			"Ensure that you have enough permissions to read it.",
+		)
+	}
+	graffiti.Println("@UPermissions@r  @UName@r")
+	for _, entry := range entries {
+		info, err := entry.Info()
+		if err != nil {
+			continue
+		}
+		mode := fmt.Sprint(info.Mode())
+		graffiti.Println("%s   %s", colorizeMode(&mode), info.Name())
+	}
+	graffiti.Println("")
+	graffiti.Println("@BPath:@r %s.", *path)
+	graffiti.Println("@BTotal:@r %d.", len(entries))
 }
 
 func main() {
@@ -38,7 +79,13 @@ func main() {
 			"Ensure that you have not mispelled it.",
 		)
 	}
-	graffiti.Println("Is directory: %v.", metadata.IsDir())
-	graffiti.Println("Path: %s.", path)
+	if metadata.IsDir() {
+		revealDirectory(&path)
+	} else {
+		throwError(
+			"Could not reveal the given path type.",
+			"Use the flag --help to see what types can be revealed.",
+		)
+	}
 }
 
