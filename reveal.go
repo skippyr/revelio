acda7c125469a7343b8f5f67a914d59c24e4da5c
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-17 20:33:19 -0300

    refactor: use struct to ease logic to calculate entry size

--- reveal.go
+++ reveal.go
@@ -9,6 +9,10 @@ import (
 	"github.com/skippyr/graffiti"
 )
 
+type DigitalSizeUnit struct {
+	valueInBytes float32
+	SiCharacter  rune
+}
 type EntryPermission struct {
 	bit       int
 	character rune
@@ -108,32 +112,34 @@ func stringifyPermissions(permissionsMode fs.FileMode) string {
 	return permissionsAsString
 }
 
-func stringifySize(sizeInBytes *int64) string {
-	if *sizeInBytes == 0 {
+func stringifySize(sizeInBytes int64) string {
+	if sizeInBytes == 0 {
 		return "       -"
 	}
-	const (
-		oneGigaByteInBytes = 1e9
-		oneMegaByteInBytes = 1e6
-		oneKiloByteInBytes = 1e3
-		floatPrecision     = 1
-		floatDigits        = 6
-		intDigits          = 7
-		unitColor          = "cyan"
-	)
-	sizeInGigaBytes := float32(*sizeInBytes) / oneGigaByteInBytes
-	if int(sizeInGigaBytes) > 0 {
-		return fmt.Sprintf("%*.*fGB", floatDigits, floatPrecision, sizeInGigaBytes)
+	units := []DigitalSizeUnit{
+		{
+			// GigaBytes
+			valueInBytes: 1e9,
+			SiCharacter:  'G',
+		},
+		{
+			// MegaBytes
+			valueInBytes: 1e6,
+			SiCharacter:  'M',
+		},
+		{
+			// KiloBytes
+			valueInBytes: 1e3,
+			SiCharacter:  'K',
+		},
 	}
-	sizeInMegaBytes := float32(*sizeInBytes) / oneMegaByteInBytes
-	if int(sizeInMegaBytes) > 0 {
-		return fmt.Sprintf("%*.*fMB", floatDigits, floatPrecision, sizeInMegaBytes)
+	for _, unit := range units {
+		unitSize := float32(sizeInBytes) / unit.valueInBytes
+		if int(unitSize) > 0 {
+			return fmt.Sprintf("%6.1f%cB", unitSize, unit.SiCharacter)
+		}
 	}
-	sizeInKiloBytes := float32(*sizeInBytes) / oneKiloByteInBytes
-	if int(sizeInKiloBytes) > 0 {
-		return fmt.Sprintf("%*.*fkB", floatDigits, floatPrecision, sizeInKiloBytes)
-	}
-	return fmt.Sprintf("%*dB", intDigits, *sizeInBytes)
+	return fmt.Sprintf("%7dB", sizeInBytes)
 }
 
 func revealDirectory(path *string) {
@@ -154,7 +160,7 @@ func revealDirectory(path *string) {
 		if info.Mode().IsRegular() {
 			sizeInBytes = info.Size()
 		}
-		size := stringifySize(&sizeInBytes)
+		size := stringifySize(sizeInBytes)
 		typeMode := stringifyType(info.Mode().Type())
 		permissionsMode := stringifyPermissions(info.Mode().Perm())
 		name := graffiti.EscapePrefixCharacters(info.Name())
