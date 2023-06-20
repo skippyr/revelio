37f0e109c23eb403f8b8c12b85d720a9d6f46beb
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-20 10:40:14 -0300

    refactor: adjust colors when printing

--- errors/errors.go
+++ errors/errors.go
@@ -8,15 +8,15 @@ import (
 
 func ThrowError(description string, suggestion string) {
 	exitCode := 1
-	graffiti.Eprintln("@F{red}Reveal - Error Report")
+	graffiti.Eprintln("@F{magenta}Reveal - Error Report")
 	graffiti.Eprintln("")
-	graffiti.Eprintln("@F{yellow}Description")
+	graffiti.Eprintln("@F{magenta}Description")
 	graffiti.Eprintln("    %s", description)
 	graffiti.Eprintln("")
-	graffiti.Eprintln("@F{yellow}Suggestion")
+	graffiti.Eprintln("@F{magenta}Suggestion")
 	graffiti.Eprintln("    %s", suggestion)
 	graffiti.Eprintln("")
-	graffiti.Eprintln("@F{yellow}Help")
+	graffiti.Eprintln("@F{magenta}Help")
 	graffiti.Eprintln("    If you need more information about usage, you can read its manual by using")
 	graffiti.Eprintln("    the manual flag:")
 	graffiti.Eprintln("")
@@ -25,7 +25,7 @@ func ThrowError(description string, suggestion string) {
 	graffiti.Eprintln("    If you can not figure out how to solve this error,")
 	graffiti.Eprintln("    You can file an issue describing what happened at:")
 	graffiti.Eprintln("")
-	graffiti.Eprintln("    @F{green}https://github.com/skippyr/reveal/issues")
+	graffiti.Eprintln("    @Uhttps://github.com/skippyr/reveal/issues")
 	graffiti.Eprintln("")
 	graffiti.Eprintln("    And the developer will help you whenever possible.")
 	graffiti.Eprintln("")
