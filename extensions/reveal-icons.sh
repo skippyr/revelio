6da105041c4e69310e65f511c7270ede74a9e3c1
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-29 13:14:52 -0300

    refactor: significantly increase speed of reveal-icons extension

--- extensions/reveal-icons.sh
+++ extensions/reveal-icons.sh
@@ -1,64 +1,44 @@
 # Reveals directories entries alphabetically with Nerd Font icons.
 function reveal-icons {
   typeset IFS=$'\n'
+  typeset -rA name_icons=(
+    ".clang-format" " "
+    ".editorconfig" " "
+    ".gitignore"    " "
+    "LICENSE"       " "
+    "Makefile"      " "
+  )
+  typeset -rA extension_icons=(
+    "zsh"       " "
+    "sh"        " "
+    "zsh-theme" "󰏘 "
+    "html"      " "
+    "xml"       "󰗀 "
+    "rb"        "󰴭 "
+    "js"        "󰌞 "
+    "css"       " "
+    "scss"      " "
+    "java"      "󰬷 "
+    "cpp"       " "
+    "hpp"       " "
+    "c"         " "
+    "h"         " "
+    "cs"        " "
+    "rs"        " "
+    "md"        " "
+    "xcf"       "󰃣 "
+    "conf"      " "
+    "mk"        " "
+    "ttf"       " "
+    "otf"       " "
+    "ufo"       " "
+    "png"       "󰈟 "
+    "jpg"       "󰈟 "
+    "bmp"       "󰈟 "
+    "o"         " "
+    "1"         " "
+  )
   typeset paths=($@)
-  function get-icon {
-    typeset -r path_="$1"
-    if [[ -d "${path_}" ]]; then
-      echo " "
-      return
-    fi
-    typeset -r name="$2"
-    typeset -r extension="$3"
-    typeset -rA name_icons=(
-      ".clang-format" " "
-      ".editorconfig" " "
-      ".gitignore"    " "
-      "LICENSE"       " "
-      "Makefile"      " "
-    )
-    typeset -rA extension_icons=(
-      "zsh"       " "
-      "sh"        " "
-      "zsh-theme" "󰏘 "
-      "html"      " "
-      "xml"       "󰗀 "
-      "rb"        "󰴭 "
-      "js"        "󰌞 "
-      "css"       " "
-      "scss"      " "
-      "java"      "󰬷 "
-      "cpp"       " "
-      "hpp"       " "
-      "c"         " "
-      "h"         " "
-      "cs"        " "
-      "rs"        " "
-      "md"        " "
-      "xcf"       "󰃣 "
-      "conf"      " "
-      "mk"        " "
-      "ttf"       " "
-      "otf"       " "
-      "ufo"       " "
-      "png"       "󰈟 "
-      "jpg"       "󰈟 "
-      "bmp"       "󰈟 "
-      "o"         " "
-      "1"         " "
-    )
-    typeset -r name_icon="${name_icons[${name}]}"
-    if [[ "${name_icon}" != "" ]]; then
-      echo "${name_icon}"
-      return
-    fi
-    typeset -r extension_icon="${extension_icons[${extension}]}"
-    if [[ "${extension_icon}" != "" ]]; then
-      echo "${extension_icon}"
-      return
-    fi
-    echo " "
-  }
   [[ ${#paths[@]} -eq 0 ]] &&
     paths+=(".")
   for path_ in ${paths[@]}; do
@@ -70,10 +50,22 @@ function reveal-icons {
     for entry in $(reveal "${path_}" | sort); do
       typeset name="${entry##*/}"
       typeset extension="${entry##*.}"
-      typeset icon="$(get-icon "${entry}" "${name}" "${extension}")"
-      echo "  ${icon} ${name}"
+      printf "  "
+      if [[ -d "${entry}" ]]; then
+        printf " "
+      else
+        typeset name_icon="${name_icons[${name}]}"
+        typeset extension_icon="${extension_icons[${extension}]}"
+        if [[ "${name_icon}" != "" ]]; then
+          printf "${name_icon}"
+        elif [[ "${extension_icon}" != "" ]]; then
+          printf "${extension_icon}"
+        else
+          printf " "
+        fi
+      fi
+      echo " ${name}"
     done
     echo
   done
-  unset -f get-icon
 }
