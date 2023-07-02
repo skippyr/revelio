007e54781ebe2274130ee7847d5816e8920ab608
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-02 17:02:30 -0300

    refactor: reduce calls to echo in reveal-stat

--- extensions/reveal-stat.sh
+++ extensions/reveal-stat.sh
@@ -6,9 +6,12 @@ function reveal-stat {
   typeset paths=($@)
   [[ ${#paths[@]} -eq 0 ]] &&
     paths+=(".")
+  typeset output=""
   for path_ in ${paths[@]}; do
+    [[ -n "${output}" ]] &&
+      output+="\n"
     if [[ ! -L "${path_}" && ! -e "${path_}" ]]; then
-      echo "$0: the path \"${path_}\" does not exists."
+      output+="$0: the path \"${path_}\" does not exists.\n"
       continue
     fi
     typeset metadata=($(reveal\
@@ -21,6 +24,7 @@ function reveal-stat {
       --group-uid ${path_}\
       --group ${path_}\
       --modified-date ${path_}))
-    echo -e "Path: \"${path_}\".  Size: ${metadata[1]}.  Inode: ${metadata[2]}.\nPermissions: (${metadata[3]}/${metadata[4]}).  Owner: (${metadata[5]}/${metadata[6]}).  Group: (${metadata[7]}/${metadata[8]}).\nModified Date: ${metadata[9]}.\n"
+    output+="Path: \"${path_}\".  Size: ${metadata[1]}.  Inode: ${metadata[2]}.\nPermissions: (${metadata[3]}/${metadata[4]}).  Owner: (${metadata[5]}/${metadata[6]}).  Group: (${metadata[7]}/${metadata[8]}).\nModified Date: ${metadata[9]}.\n"
   done
+  echo "${output}"
 }
