4de93330f0476c56d76c7c56198250ec8dc639e2
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-10 14:28:21 -0300

    refactor: add function to parse type

--- reveal.c
+++ reveal.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +7,7 @@
 
 #define programName "reveal"
 #define programLicense "Copyright (c) 2023, Sherman Rofeman. MIT license."
-#define programVersion "v5.2.2"
+#define programVersion "v6.0.0"
 #define programIssuesPage "https://github.com/skippyr/" programName "/issues"
 #define programHelp "Usage: " programName " [FLAGS]... [PATHS]...\nReveals "   \
                     "information about entries in the file system.\n\nMETADATA"\
@@ -16,30 +17,32 @@
                     "\nDATA TYPE FLAGS\nThese flags change what data type the "\
                     "program will reveal from the entries.\n\n  --contents "   \
                     "(default)  prints its contents.\n  --type                "\
-                    "prints its type.\n  --size                prints its size"\
-                    ".\n  --human-size          prints its size using a "      \
-                    "formidable unit.\n  --blocks              prints the "    \
-                    "quantity of blocks it takes.\n  --hard-links          "   \
-                    "prints the quantity of hard links it has.\n  --user      "\
-                    "          prints the user that owns it.\n  --user-id     "\
-                    "        prints the ID of the user that owns it.\n  "      \
-                    "--group               prints the group that owns it.\n  " \
-                    "--group-id            prints the ID of the group that "   \
-                    "owns it.\n  --mode                prints a bit set "      \
-                    "containing its properties and\n                        "  \
-                    "permissions.\n  --permissions         prints its "        \
-                    "permissions in octal base.\n  --human-permissions   "     \
-                    "prints its permissions for user, group, and others,\n    "\
-                    "                    respectively, using three set of "    \
-                    "characters each:\n                        read (r), write"\
-                    " (w), execute (x) and lack (-).\n  --inode               "\
-                    "prints its serial number.\n  --modified-date       prints"\
-                    " the date where its contents were last modified.\n  "     \
-                    "--changed-date        prints the date where its metadata "\
-                    "in the file system's was\n                        last "  \
-                    "changed.\n  --accessed-date       prints the date where " \
-                    "its contents were last accessed.\n\nTRANSPASSING FLAGS\n" \
-                    "These flags changes the way symlinks are handled.\n\n  "  \
+                    "prints its type: block, character, directory, fifo,\n"    \
+                    "                        symlink, regular or unknown.\n  " \
+                    "--size                prints its size in bytes.\n  "      \
+                    "--human-size          prints its size using a formidable "\
+                    "unit.\n  --blocks              prints the quantity of "   \
+                    "blocks it takes.\n  --hard-links          prints the "    \
+                    "quantity of hard links it has.\n  --user                " \
+                    "prints the user that owns it.\n  --user-id             "  \
+                    "prints the ID of the user that owns it.\n  --group       "\
+                    "        prints the group that owns it.\n  --group-id     "\
+                    "       prints the ID of the group that owns it.\n  --mode"\
+                    "                prints a bit set containing its "         \
+                    "properties and\n                        permissions.\n  " \
+                    "--permissions         prints its permissions in octal "   \
+                    "base.\n  --human-permissions   prints its permissions for"\
+                    " user, group, and others,\n                        "      \
+                    "respectively, using three set of characters each:\n      "\
+                    "                  read (r), write (w), execute (x) and "  \
+                    "lack (-).\n  --inode               prints its serial "    \
+                    "number.\n  --modified-date       prints the date where "  \
+                    "its contents were last modified.\n  --changed-date       "\
+                    " prints the date where its metadata in the file system's" \
+                    "\n                        inode table was last changed.\n"\
+                    "  --accessed-date       prints the date where its "       \
+                    "contents were last accessed.\n\nTRANSPASSING FLAGS\nThese"\
+                    " flags changes the way symlinks are handled.\n\n  "       \
                     "--untranspass (default)  does not resolve symlinks.\n  "  \
                     "--transpass              resolves all levels of symlinks."\
                     "\n\nEXIT CODES\nIt will throw exit code 1 in the end of " \
@@ -60,7 +63,8 @@
 #define ParseFunctionCase(value, function)                                     \
     case value:                                                                \
         function;                                                              \
-        break;                                                                 \
+        break;
+#define ParsePutsCase(value, text) ParseFunctionCase(value, puts(text))
 
 uint8_t globalOptions = 0;
 
@@ -70,30 +74,97 @@ void PrintSplittedError(char *start, char *middle, char *end)
     globalOptions |= 1 << 7;
 }
 
+void RevealType(struct stat *metadata)
+{
+    switch (metadata->st_mode & S_IFMT)
+    {
+    ParsePutsCase(S_IFBLK, "Block")
+    ParsePutsCase(S_IFCHR, "Character")
+    ParsePutsCase(S_IFDIR, "Directory")
+    ParsePutsCase(S_IFIFO, "Fifo")
+    ParsePutsCase(S_IFLNK, "Symlink")
+    ParsePutsCase(S_IFREG, "Regular")
+    ParsePutsCase(S_IFSOCK, "Socket");
+    default:
+        puts("Unknown");
+    }
+}
+
+void RevealFile(char *path)
+{
+    FILE *const file = fopen(path, "r");
+    if (!file)
+    {
+        PrintSplittedError("could not open file \"", path, "\". Do you have "
+                           "enough permissions?");
+        return;
+    }
+    char character;
+    while ((character = fgetc(file)) != EOF)
+        putchar(character);
+    fclose(file);
+}
+
+void RevealDirectory(char *path)
+{
+    char absolutePath[PATH_MAX];
+    if (!realpath(path, absolutePath))
+    {
+        PrintSplittedError("could not resolve absolute path of \"", path,
+                           "\".");
+        return;
+    }
+    DIR *const directory = opendir(path);
+    if (!directory)
+    {
+        PrintSplittedError("could not open directory \"", path, "\". Do you "
+                           "have enough permissions?");
+        return;
+    }
+    const struct dirent *entry;
+    while ((entry = readdir(directory)))
+    {
+        if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
+            continue;
+        printf("%s%s%s\n", absolutePath, !strcmp(absolutePath, "/") ? "" : "/",
+               entry->d_name);
+    }
+    closedir(directory);
+}
+
 void Reveal(char *path)
 {
     struct stat metadata;
     if (globalOptions & 1 << 6 ? stat(path, &metadata) : lstat(path, &metadata))
     {
         PrintSplittedError("the path \"", path, "\" does not points to "
-                           "anything. Did you not mispelled it?\n");
+                           "anything. Did you not mispelled it?");
         return;
     }
     switch (globalOptions & ~(1 << 6 | 1 << 7))
     {
-
+    ParseFunctionCase(1, RevealType(&metadata))
+    default:
+        switch (metadata.st_mode & S_IFMT)
+        {
+            ParseFunctionCase(S_IFREG, RevealFile(path))
+            ParseFunctionCase(S_IFDIR, RevealDirectory(path))
+        default:
+            PrintSplittedError("can not reveal the contents of \"", path,
+                               "\" type.");
+        }
     }
 }
 
 int main(int quantityOfArguments, char **arguments)
 {
-    for (int i = 0; i < quantityOfArguments; i++)
+    for (int i = 1; i < quantityOfArguments; i++)
     {
         ParseMetadataFlag("license", puts(programLicense))
         ParseMetadataFlag("version", puts(programVersion))
         ParseMetadataFlag("help", puts(programHelp))
     }
-    for (int i = 0; i < quantityOfArguments; i++)
+    for (int i = 1; i < quantityOfArguments; i++)
     {
         ParseDataTypeFlag("contents", 0)
         ParseDataTypeFlag("type", 1)
