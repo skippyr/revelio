ec31c497d61fa082f9ce65e121d637fd56e883b7
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-12 11:24:59 -0300

    refactor: add remaining functions

--- reveal.c
+++ reveal.c
@@ -1,15 +1,22 @@
 #include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #define programName "reveal"
 #define programVersion "v7.0.0"
 #define programLicense "Copyright (c) 2023, Sherman Rofeman. MIT license."
 #define programHelp\
     "Usage: " programName " [FLAGS, PATHS...]"
+#define readCharacter 'r'
+#define writeCharacter 'w'
+#define executeCharacter 'x'
+#define lackCharacter '-'
 #define isTranspassingBit (1 << 6)
 #define exitCodeBit (1 << 7)
 #define nonDataTypeBits (isTranspassingBit | exitCodeBit)
@@ -30,11 +37,30 @@
         function;                                                              \
         break;
 #define ParsePutsCase(value, text) ParseFunctionCase(value, puts(text))
+#define ParseSize(multiplier, unit)                                            \
+    size = metadata->st_size / (multiplier);                                   \
+    if ((int) size)                                                            \
+    {                                                                          \
+        printf("%.1f%s\n", size, unit);                                        \
+        return;                                                                \
+    }
+#define ParsePermission(permission, character)\
+    putchar(metadata->st_mode & permission ? character : lackCharacter);
+#define PrintUnsignedValue(value) printf("%u\n", value);
+#define PrintLongValue(value) printf("%ld\n", value);
+#define PrintUnsignedLongValue(value) printf("%lu\n", value);
 
 static void Reveal(const char *const path);
 static void RevealFile(const char *const path);
 static void RevealDirectory(const char *const path);
 static void RevealType(const struct stat *const metadata);
+static void RevealUser(const struct stat *const metadata,
+                       const char *const path);
+static void RevealGroup(const struct stat *const metadata,
+                        const char *const path);
+static void RevealDate(const time_t *const date);
+static void RevealHumanSize(const struct stat *const metadata);
+static void RevealHumanPermissions(const struct stat *const metadata);
 static void PrintSplittedError(const char *const descriptionStart,
                                const char *const descriptionMiddle,
                                const char *const descriptionEnd);
@@ -93,24 +119,23 @@ Reveal(const char *const path)
     switch (globalOptions & ~nonDataTypeBits)
     {
     ParseFunctionCase(1, RevealType(&metadata))
-    // ParseFunctionCase(2, PrintLongValue(metadata.st_size))
-    // ParseFunctionCase(3, RevealHumanSize(&metadata))
-    // ParseFunctionCase(4, PrintLongValue(metadata.st_blocks))
-    // ParseFunctionCase(5, PrintUnsignedLongValue(metadata.st_nlink))
-    // ParseFunctionCase(6, RevealUser(&metadata, path))
-    // ParseFunctionCase(7, PrintUnsignedValue(metadata.st_uid))
-    // ParseFunctionCase(8, RevealGroup(&metadata, path))
-    // ParseFunctionCase(9, PrintUnsignedValue(metadata.st_gid))
-    // ParseFunctionCase(10, PrintUnsignedValue(metadata.st_mode))
-    // ParseFunctionCase(11, printf("0%o\n", metadata.st_mode &
-    //                              (S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP |
-    //                               S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH |
-    //                               S_IXOTH)))
-    // ParseFunctionCase(12, RevealHumanPermissions(&metadata))
-    // ParseFunctionCase(13, PrintUnsignedLongValue(metadata.st_ino))
-    // ParseFunctionCase(14, RevealDate(&metadata.st_mtime))
-    // ParseFunctionCase(15, RevealDate(&metadata.st_ctime))
-    // ParseFunctionCase(16, RevealDate(&metadata.st_atime))
+    ParseFunctionCase(2, PrintLongValue(metadata.st_size))
+    ParseFunctionCase(3, RevealHumanSize(&metadata))
+    ParseFunctionCase(4, PrintLongValue(metadata.st_blocks))
+    ParseFunctionCase(5, PrintUnsignedLongValue(metadata.st_nlink))
+    ParseFunctionCase(6, RevealUser(&metadata, path))
+    ParseFunctionCase(7, PrintUnsignedValue(metadata.st_uid))
+    ParseFunctionCase(8, RevealGroup(&metadata, path))
+    ParseFunctionCase(9, PrintUnsignedValue(metadata.st_gid))
+    ParseFunctionCase(10, PrintUnsignedValue(metadata.st_mode))
+    ParseFunctionCase(11, printf("0%o\n", metadata.st_mode & S_IRUSR | S_IWUSR |
+                                 S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP |
+                                 S_IROTH | S_IWOTH | S_IXOTH))
+    ParseFunctionCase(12, RevealHumanPermissions(&metadata))
+    ParseFunctionCase(13, PrintUnsignedLongValue(metadata.st_ino))
+    ParseFunctionCase(14, RevealDate(&metadata.st_mtime))
+    ParseFunctionCase(15, RevealDate(&metadata.st_ctime))
+    ParseFunctionCase(16, RevealDate(&metadata.st_atime))
     default:
         switch (metadata.st_mode & S_IFMT)
         {
@@ -127,7 +152,7 @@ Reveal(const char *const path)
 static void
 RevealFile(const char *const path)
 {
-    FILE *file = fopen(path, "r");
+    FILE *const file = fopen(path, "r");
     if (!file)
     {
         PrintSplittedError("could not open file \"", path, "\". Do you have "
@@ -151,14 +176,14 @@ RevealDirectory(const char *const path)
                            "\".");
         return;
     }
-    DIR *directory = opendir(path);
+    DIR *const directory = opendir(path);
     if (!directory)
     {
         PrintSplittedError("could not open directory \"", path, "\". Do you "
                            "have enough permissions?");
         return;
     }
-    struct dirent *entry;
+    const struct dirent *entry;
     while ((entry = readdir(directory)))
     {
         if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
@@ -189,6 +214,67 @@ RevealType(const struct stat *const metadata)
 }
 
 static void
+RevealHumanSize(const struct stat *const metadata)
+{
+    float size;
+    ParseSize(1e9, "GB");
+    ParseSize(1e6, "MB");
+    ParseSize(1e3, "KB");
+    printf("%ldB\n", metadata->st_size);
+    return;
+}
+
+static void
+RevealUser(const struct stat *const metadata, const char *const path)
+{
+    const struct passwd *const user = getpwuid(metadata->st_uid);
+    if (user)
+        puts(user->pw_name);
+    else
+        PrintSplittedError("could not get user that owns \"", path, "\".");
+    return;
+}
+
+static void
+RevealGroup(const struct stat *const metadata, const char *const path)
+{
+    const struct group *const group = getgrgid(metadata->st_gid);
+    if (group)
+        puts(group->gr_name);
+    else
+        PrintSplittedError("could not get group that owns \"", path, "\".");
+    return;
+}
+
+static void
+RevealDate(const time_t *const date)
+{
+    char formattedDate[29];
+    if (strftime(formattedDate, sizeof(formattedDate), "%a %b %d %T %Z %Y",
+        localtime(date)))
+        puts(formattedDate);
+    else
+        PrintSplittedError("overflowed buffer to store date.", "", "");
+    return;
+}
+
+static void
+RevealHumanPermissions(const struct stat *const metadata)
+{
+    ParsePermission(S_IRUSR, readCharacter)
+    ParsePermission(S_IWUSR, writeCharacter)
+    ParsePermission(S_IXUSR, executeCharacter)
+    ParsePermission(S_IRGRP, readCharacter)
+    ParsePermission(S_IWGRP, writeCharacter)
+    ParsePermission(S_IXGRP, executeCharacter)
+    ParsePermission(S_IROTH, readCharacter)
+    ParsePermission(S_IWOTH, writeCharacter)
+    ParsePermission(S_IXOTH, executeCharacter)
+    putchar('\n');
+    return;
+}
+
+static void
 PrintSplittedError(const char *const descriptionStart,
                    const char *const descriptionMiddle,
                    const char *const descriptionEnd)
@@ -198,3 +284,4 @@ PrintSplittedError(const char *const descriptionStart,
     globalOptions |= exitCodeBit;
     return;
 }
+
