638278c86a526e9a27dc6c5845bec3b76462c73b
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-28 22:20:29 -0300

    ~% Add parse of data type flags.

--- reveal.c
+++ reveal.c
@@ -20,6 +20,9 @@
     "If none is used, the one marked as default will be considered in "        \
     "use.\n\n"                                                                 \
     "    --contents (default)    prints its contents.\n"                       \
+    "    --type                  prints its type: regular, directory, "        \
+    "symlink,\n                            socket, fifo, block, character or " \
+    "unknown.\n"                                                               \
     "    --size                  prints its size using the most formidable "   \
     "unit:\n                            gigabyte (GB), megabyte (MB), "        \
     "kilobyte (KB) "                                                           \
@@ -50,20 +53,89 @@
     "Errors will be reported through standard error stream.\n\n"               \
     "SUPPORT\n"                                                                \
     "Report issues, questions or suggestion at:\n" program_support__ "."
+#define is_expecting_path_bit__ (1 << 5)
+#define is_following_symlinks_bit__ (1 << 6)
+#define had_error_bit__ (1 << 7)
+#define non_data_type_bits__                                                   \
+    (is_expecting_path_bit__ | is_following_symlinks_bit__ | had_error_bit__)
+#define Is_Last_Argument__(argument_index)                                     \
+    (argument_index == total_of_arguments - 1)
+#define Reveal_Last_Path__                                                     \
+    Reveal(last_path_index == -1 ? "." : arguments[last_path_index])
 #define Parse_Flag__(flag, action)                                             \
-    if (!strcmp("--" flag, arguments[i]))                                      \
+    if (!strcmp("--" flag, arguments[argument_index]))                         \
     {                                                                          \
         action                                                                 \
     }
 #define Parse_Metadata_Flag__(flag, text)                                      \
     Parse_Flag__(flag, puts(text); return (0);)
+#define Parse_Data_Type_Flag__(flag, data_type)                                \
+    Parse_Flag__(                                                              \
+        flag,                                                                  \
+        if (global_options & is_expecting_path_bit__) {                        \
+            Reveal_Last_Path__;                                                \
+        } global_options = (data_type | is_expecting_path_bit__ |              \
+                            (global_options & non_data_type_bits__));          \
+        continue;)
 
-int main(const int total_of_arguments, const char **arguments)
+typedef enum
 {
-    for (int i = 1; i < total_of_arguments; i++)
+    Data_Type_Contents,
+    Data_Type_Type,
+    Data_Type_Size,
+    Data_Type_Byte_Size,
+    Data_Type_Permissions,
+    Data_Type_Octal_Permission,
+    Data_Type_User,
+    Data_Type_User_Uid,
+    Data_Type_Group,
+    Data_Type_Group_Gid,
+    Data_Type_Modified_Date
+} Data_Type;
+
+uint8_t global_options = is_following_symlinks_bit__;
+
+void
+Reveal(const char *const path)
+{
+    printf("%s (mode: %i) (is following symlinks: %i)\n", path,
+           global_options & ~non_data_type_bits__,
+           global_options & is_following_symlinks_bit__);
+    return;
+}
+
+int
+main(const int total_of_arguments, const char **arguments)
+{
+    if (total_of_arguments == 1)
+    {
+        Reveal(".");
+    }
+    int last_path_index = -1;
+    for (int argument_index = 1; argument_index < total_of_arguments;
+         argument_index++)
     {
         Parse_Metadata_Flag__("copyright", program_copyright__);
         Parse_Metadata_Flag__("version", program_version__);
         Parse_Metadata_Flag__("help", program_help__);
     }
+    for (int argument_index = 1; argument_index < total_of_arguments;
+         argument_index++)
+    {
+        Parse_Data_Type_Flag__("contents", Data_Type_Contents);
+        Parse_Data_Type_Flag__("type", Data_Type_Type);
+        Parse_Data_Type_Flag__("size", Data_Type_Size);
+        Parse_Data_Type_Flag__("byte-size", Data_Type_Byte_Size);
+        Parse_Data_Type_Flag__("permissions", Data_Type_Permissions);
+        Parse_Data_Type_Flag__("octal-permissions", Data_Type_Octal_Permission);
+        Parse_Data_Type_Flag__("user", Data_Type_User);
+        Parse_Data_Type_Flag__("user-uid", Data_Type_User_Uid);
+        Parse_Data_Type_Flag__("group", Data_Type_Group);
+        Parse_Data_Type_Flag__("group-gid", Data_Type_Group_Gid);
+        Parse_Data_Type_Flag__("modified-date", Data_Type_Modified_Date);
+        global_options &= ~is_expecting_path_bit__;
+        last_path_index = argument_index;
+        Reveal_Last_Path__;
+    }
+    return !!(global_options & had_error_bit__);
 }
