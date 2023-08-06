de49d79910669bd9d236f6dd6c1499a25bc786ea
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-06 09:21:20 -0300

    ~% add function to reveal modified date

--- reveal.c
+++ reveal.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #define program_name__ "reveal"
 #define program_version__ "version"
@@ -156,6 +157,18 @@ uint8_t Reveal_Group(const struct stat* const metadata, const char* const path)
     return 0;
 }
 
+uint8_t Reveal_Modified_Date(const struct stat* const metadata)
+{
+    char modified_date[29];
+    if (!strftime(modified_date, sizeof(modified_date), "%a %b %d %T %Z %Y",
+        localtime(&metadata->st_mtime))) {
+        return Throw_Error("overflowed buffer meant to store modified date.",
+                           NULL, NULL, NULL);
+    }
+    puts(modified_date);
+    return 0;
+}
+
 uint8_t Reveal_File(const char* const path)
 {
     FILE* const file = fopen(path, "r");
@@ -207,6 +220,8 @@ uint8_t Reveal(const char* const path)
         Parse_Case__(Data_Type_User_Uid, Print_Unsigned__(metadata.st_uid));
         Parse_Return_Case__(Data_Type_Group, Reveal_Group(&metadata, path));
         Parse_Case__(Data_Type_Group_Gid, Print_Unsigned__(metadata.st_gid));
+        Parse_Return_Case__(Data_Type_Modified_Date,
+                            Reveal_Modified_Date(&metadata));
     default:
         switch (metadata.st_mode & S_IFMT) {
             Parse_Return_Case__(S_IFREG, Reveal_File(path));
