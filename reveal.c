ac16cd7054e5eece9dc130f10a7e32fafa13fa22
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-12 19:11:32 -0300

    ~% rewrite help

--- reveal.c
+++ reveal.c
@@ -8,8 +8,45 @@
 #include <time.h>
 
 #define program_name__ "reveal"
-#define program_version__ "v9.0.5"
-#define program_help__ "help"
+#define program_version__ "v9.0.6"
+#define program_help__\
+	"Usage: " program_name__ " [OPTION]... [PATH]...\n"\
+	"Reveals information about entries in the file system.\n\n"\
+	"METADATA OPTIONS\n"\
+	"These options retrive information about the program.\n\n"\
+	"    --help       print these help instructions.\n"\
+	"    --version    print its version.\n\n"\
+	"DATA TYPE OPTIONS\n"\
+	"These options change the data type retrived from the entries following "\
+	"them.\n\n"\
+	"    --contents (default)    print its contents.\n"\
+	"    --type                  print its type: regular, directory, symlink, "\
+	"socket,\n                            fifo, character, block or unknown.\n"\
+	"    --size                  print its size using a convenient unit: "\
+	"gigabyte (GB),\n                            megabyte (MB), kilobyte (kB) "\
+	"or byte (B).\n"\
+	"    --byte-size             print its size in bytes.\n"\
+	"    --permissions           print its read (r), write (w) and execute (x)"\
+	"\n                            permissions for user, group and others.\n"\
+	"    --octal-permissions     print its permissions using octal base.\n"\
+	"    --user                  print the user that owns it.\n"\
+	"    --user-uid              print the UID of the user that owns it.\n"\
+	"    --group                 print the group that owns it.\n"\
+	"    --group-gid             print the GID of the group that owns it.\n"\
+	"    --modified-date         print the date when its contents were last "\
+	"modified.\n\n"\
+	"If none is used, the one marked as default will be considered.\n\n"\
+	"Each one of them expects at least one path following it. If none is "\
+	"given, it will\nconsider the last valid one given or, else, the current "\
+	"directory.\n\n"\
+	"SYMLINK FLAGS\n"\
+	"These options change how the symlinks following them are handled, "\
+	"changing the\norigin of the data revealed.\n\n"\
+	"    --follow-symlinks (default)    symlinks will be followed.\n"\
+	"    --unfollow-symlinks            symlinks will not be followed.\n\n"\
+	"If none is used, the one marked as default will be considered.\n\n"\
+	"EXIT CODES\n"\
+	"It will throw code 1 if an error happens and 0 otherwise."
 #define is_expecting_path_bit__ (1 << 5)
 #define is_following_symlinks_bit__ (1 << 6)
 #define had_error_bit__ (1 << 7)
