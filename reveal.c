53adbd7e032faceb9ccf32a3be0100fa5b0b1a19
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-27 09:24:34 -0300

    Add changes for release v11.0.0
    This commits adds all the changes prototype for the next release,
    `v11.0.0`:
    
    - The exit codes section in the help page will now show the values of
    `EXIT_FAILURE` and `EXIT_SUCCESS` for the system it was compiled for instead of
    hard-coded values.
    - The error report for entries not found has been slightly changed.
    - Naming convention for functions, variables and macros has been changed to
    better match the one already used in the C libraries.

--- reveal.c
+++ reveal.c
@@ -8,73 +8,42 @@
 #include <sys/stat.h>
 #include <time.h>
 
-#define PGR_NAME "reveal"
-#define PGR_VRS "v11.0.0"
-#define EXIT_CODE(e) (e ? EXIT_FAILURE : EXIT_SUCCESS)
-#define NULL_STR(s) (s ? s : "")
-#define RET_CASE(val, act)\
-case val:\
-	return act;
-#define CASE(val, act)\
-case val:\
-	act;\
-	break;
-#define PUTS_CASE(val, txt) CASE(val, puts(txt))
-#define PUT_PERM_CHR(perm, chr) putchar(s->st_mode & perm ? chr : '-')
-#define PARSE_SIZE_PREF_MUL(mul, chr)\
-z = s->st_size / (mul);\
-if ((int)z) {\
-	printf("%.1f%cB\n", z, chr);\
-	return;\
-}
-#define PARSE_OPT(opt, tgt, act)\
-if (!strcmp("-" opt, tgt)) {\
-	act;\
-}
-#define PARSE_META_OPT(opt, act)\
-PARSE_OPT(opt, args[i],\
-act;\
-exit(EXIT_SUCCESS))
-#define PARSE_DT_OPT(opt, dt)\
-PARSE_OPT(opt, arg,\
-if (AW_ARG) {\
-	reveal(path);\
-}\
-DT = dt;\
-AW_ARG = true;\
-if (is_last_arg) {\
-	reveal(path);\
-}\
-return 1)
-#define PARSE_LNK_OPT(opt, fl)\
-PARSE_OPT(opt, arg,\
-if (is_last_arg) {\
-	reveal(path);\
-}\
-FL_LNK = fl;\
-return 1)
+#define PGRNAME "reveal"
+#define PGRVRS "v11.0.1"
+#define PARSEEXITCD(e) (e ? EXIT_FAILURE : EXIT_SUCCESS)
+#define PARSENULLSTR(s) (s ? s : "")
+#define PARSERETCASE(v, a) case v: return a;
+#define PARSECASE(v, a) case v: a; break;
+#define PARSEPUTSCASE(v, t) PARSECASE(v, puts(t))
+#define PARSESIZEPREFMUL(m, c) z = s->st_size / (m); if ((int)z) {\
+printf("%.1f%cB\n", z, c); return;}
+#define PARSEPERM(p, c) putchar(s->st_mode & p ? c : '-')
+#define PARSEOPT(o, t, a) if (!strcmp("-" o, t)) {a;}
+#define PARSEMETAOPT(o, a) PARSEOPT(o, args[i], a; exit(EXIT_SUCCESS))
+#define PARSEDTOPT(o, d) PARSEOPT(o, arg, if (AWARG) {reveal(path);} DT = d;\
+AWARG = true; if (islast) {reveal(path);} return 1;)
+#define PARSELNKOPT(o, f) PARSEOPT(o, arg, if (islast) {reveal(path);}\
+FLLNK = f; return 1;)
 
-enum data_type {
+enum datatype {
 	DT_CTTS,
 	DT_TYPE,
 	DT_SIZE,
-	DT_BT_SIZE,
+	DT_BSIZE,
 	DT_PERMS,
-	DT_OCT_PERMS,
+	DT_OPERMS,
 	DT_USR,
-	DT_USR_ID,
+	DT_UID,
 	DT_GRP,
-	DT_GRP_ID,
-	DT_MOD_DATE
+	DT_GID,
+	DT_MDATE
 };
 
-static enum data_type DT = DT_CTTS;
-static bool H_ERR = false, FL_LNK = true, AW_ARG = false;
+static enum datatype DT = DT_CTTS;
+static bool HERR = false, FLLNK = true, AWARG = false;
 
-static void
-help(void)
-{
-	printf("Usage: %s [OPTION | PATH]...\n", PGR_NAME);
+static void help(void) {
+	printf("Usage: %s [OPTION | PATH]...\n", PGRNAME);
 	puts("Reveals info about entries in the file system.\n");
 	puts("META OPTIONS");
 	puts("These options retrieve information about the program.\n");
@@ -112,7 +81,8 @@ help(void)
 	puts("If none of these is provided, the one marked as default will be "
 	"considered.\n");
 	puts("EXIT CODES");
-	puts("Code 1 will be throw if an error happens and 0 otherwise.\n");
+	printf("Code %i will be throw if an error happens and %i "
+	"otherwise.\n\n", EXIT_FAILURE, EXIT_SUCCESS);
 	puts("SOURCE CODE");
 	puts("Its source code is available at:");
 	puts("<https://github.com/skippyr/reveal>.\n");
@@ -122,289 +92,224 @@ help(void)
 	puts("<https://github.com/skippyr/reveal/issues>");
 }
 
-static int
-print_err(char *desc0, char *desc1, char *desc2, char *fix)
-{
-	fprintf(stderr, "%s: %s%s%s\n%s%s", PGR_NAME, NULL_STR(desc0),
-	NULL_STR(desc1), NULL_STR(desc2), NULL_STR(fix), fix ? "\n" : "");
-	H_ERR = true;
-	return 1;
+static int printerr(char *desc0, char *desc1, char *desc2, char *fix) {
+	return (HERR = fprintf(stderr, "%s: %s%s%s\n%s%s", PGRNAME,
+	PARSENULLSTR(desc0), PARSENULLSTR(desc1), PARSENULLSTR(desc2),
+	PARSENULLSTR(fix), fix ? "\n" : ""));
 }
 
-static void
-throw_err(char *desc)
-{
-	print_err(desc, NULL, NULL, NULL);
+static void throwerr(char *desc) {
+	printerr(desc, NULL, NULL, NULL);
 	exit(EXIT_FAILURE);
 }
 
-static void
-reveal_type(struct stat *s)
-{
+static void revealtype(struct stat *s) {
 	switch (s->st_mode & S_IFMT) {
-		PUTS_CASE(S_IFREG, "r");
-		PUTS_CASE(S_IFDIR, "d");
-		PUTS_CASE(S_IFLNK, "l");
-		PUTS_CASE(S_IFSOCK, "s");
-		PUTS_CASE(S_IFIFO, "f");
-		PUTS_CASE(S_IFCHR, "c");
-		PUTS_CASE(S_IFBLK, "b");
+		PARSEPUTSCASE(S_IFREG, "r");
+		PARSEPUTSCASE(S_IFDIR, "d");
+		PARSEPUTSCASE(S_IFLNK, "l");
+		PARSEPUTSCASE(S_IFSOCK, "s");
+		PARSEPUTSCASE(S_IFIFO, "f");
+		PARSEPUTSCASE(S_IFCHR, "c");
+		PARSEPUTSCASE(S_IFBLK, "b");
 	default:
 		puts("-");
 	}
 }
 
-static void
-reveal_size(struct stat *s)
-{
+static void revealsize(struct stat *s) {
 	float z;
-	PARSE_SIZE_PREF_MUL(1e9, 'G');
-	PARSE_SIZE_PREF_MUL(1e6, 'M');
-	PARSE_SIZE_PREF_MUL(1e3, 'k');
+	PARSESIZEPREFMUL(1e9, 'G');
+	PARSESIZEPREFMUL(1e6, 'M');
+	PARSESIZEPREFMUL(1e3, 'k');
 	printf("%ldB\n", s->st_size);
 }
 
-static void
-reveal_bt_size(struct stat *s)
-{
+static void revealbsize(struct stat *s) {
 	printf("%ld\n", s->st_size);
 }
 
-static void
-reveal_perms(struct stat *s)
-{
-	PUT_PERM_CHR(S_IRUSR, 'r');
-	PUT_PERM_CHR(S_IWUSR, 'w');
-	PUT_PERM_CHR(S_IXUSR, 'x');
-	PUT_PERM_CHR(S_IRGRP, 'r');
-	PUT_PERM_CHR(S_IWGRP, 'w');
-	PUT_PERM_CHR(S_IXGRP, 'x');
-	PUT_PERM_CHR(S_IROTH, 'r');
-	PUT_PERM_CHR(S_IWOTH, 'w');
-	PUT_PERM_CHR(S_IXOTH, 'x');
+static void revealperms(struct stat *s) {
+	PARSEPERM(S_IRUSR, 'r');
+	PARSEPERM(S_IWUSR, 'w');
+	PARSEPERM(S_IXUSR, 'x');
+	PARSEPERM(S_IRGRP, 'r');
+	PARSEPERM(S_IWGRP, 'w');
+	PARSEPERM(S_IXGRP, 'x');
+	PARSEPERM(S_IROTH, 'r');
+	PARSEPERM(S_IWOTH, 'w');
+	PARSEPERM(S_IXOTH, 'x');
 	putchar('\n');
 }
 
-static void
-reveal_oct_perms(struct stat *s)
-{
+static void revealoperms(struct stat *s) {
 	printf("%o\n", s->st_mode & (S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP |
 	S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH));
 }
 
-static int
-reveal_usr(struct stat *s, char *path)
-{
+static int revealusr(char *path, struct stat *s) {
 	struct passwd *u = getpwuid(s->st_uid);
-	if (!u)
-		return print_err("can't find user that owns \"", path, "\".",
-		"Ensure that it is not a dangling symlink.");
-	puts(u->pw_name);
-	return 0;
+	return u ? puts(u->pw_name) == EOF : printerr("can't find user that "
+	"owns \"", path, "\".", "Ensure that it is not a dangling symlink.");
 }
 
-static int
-reveal_grp(struct stat *s, char *path)
-{
+static int revealgrp(char *path, struct stat *s) {
 	struct group *g = getgrgid(s->st_gid);
-	if (!g)
-		return print_err("can't find group that owns \"", path, "\".",
-		"Ensure that it is not a dangling symlink.");
-	puts(g->gr_name);
-	return 0;
+	return g ? puts(g->gr_name) == EOF : printerr("can't find group that "
+	"owns \"", path, "\".", "Ensure that it is not a dangling symlink.");
 }
 
-static void
-reveal_own_id(unsigned i)
-{
+static void revealownid(unsigned i) {
 	printf("%u\n", i);
 }
 
-static int
-reveal_mod_date(struct stat *s)
-{
-	char m[29];
-	if (!strftime(m, sizeof(m), "%a %b %d %T %Z %Y",
-	localtime(&s->st_mtime)))
-		return print_err("overflowed modified date buffer.", NULL, NULL,
-		NULL);
-	puts(m);
-	return 0;
-}
-
-static int
-reveal_file(char *path)
-{
+static int revealfile(char *path) {
 	FILE *f = fopen(path, "r");
 	if (!f)
-		return print_err("can't open file \"", path, "\"", "Check if "
+		return printerr("can't open file \"", path, "\"", "Check if "
 		"you have permission to read it.");
-	char c;
-	while ((c = fgetc(f)) != EOF)
-		putchar(c);
-	fclose(f);
-	return 0;
+	for (char c; (c = fgetc(f)) != EOF; putchar(c));
+	return fclose(f);
 }
 
-static int
-get_tot_dir_ents(DIR *d)
-{
-	size_t s = 0;
-	for (; readdir(d); s++);
-	return s -= 2;
+static size_t gettotdirents(DIR *d) {
+	size_t t = 0;
+	for (; readdir(d); t++);
+	return t -= 2;
 }
 
-static void
-alloc_dir_ents(DIR *d, void **buf)
-{
-	struct dirent *e;
+static void allocdirents(DIR *d, void **buf) {
 	size_t i = 0;
-	while ((e = readdir(d))) {
+	for (struct dirent *e; (e = readdir(d));) {
 		if (!strcmp(e->d_name, ".") || !strcmp(e->d_name, ".."))
 			continue;
-		size_t s = strlen(e->d_name) + 1;
-		void *a = malloc(s);
+		size_t l = strlen(e->d_name) + 1;
+		void *a = malloc(l);
 		if (!a)
-			throw_err("can't allocate memory.");
-		memcpy(a, e->d_name, s);
+			throwerr("can't allocate memory.");
+		memcpy(a, e->d_name, l);
 		buf[i] = a;
 		i++;
 	}
 }
 
-static void
-sort_dir_ents(void **buf, size_t buf_len)
-{
-	for (size_t i = 0; i < buf_len - 1; i++) {
+static void sortdirents(void **buf, size_t buflen) {
+	for (size_t i = 0; i < buflen - 1; i++) {
 		size_t w = i;
-		for (size_t j = i + 1; j < buf_len; j++)
+		for (size_t j = i + 1; j < buflen; j++)
 			if (strcmp(buf[j], buf[w]) < 0)
 				w = j;
-		if (w == i)
-			continue;
 		void *t = buf[i];
 		buf[i] = buf[w];
 		buf[w] = t;
 	}
 }
 
-static int
-reveal_dir(char *path)
-{
+static int revealdir(char *path) {
 	DIR *d = opendir(path);
 	if (!d)
-		return print_err("can't open directory \"", path, "\"", "Check "
+		return printerr("can't open directory \"", path, "\"", "Check "
 		"if you have permission to read it.");
-	size_t s = get_tot_dir_ents(d);
-	if (!s) {
-		closedir(d);
-		return 0;
-	}
-	void *e[s];
+	size_t t = gettotdirents(d);
+	if (!t)
+		return closedir(d);
+	void *e[t];
 	rewinddir(d);
-	alloc_dir_ents(d, e);
-	sort_dir_ents(e, s);
-	for (size_t i = 0; i < s; i++) {
+	allocdirents(d, e);
+	sortdirents(e, t);
+	for (size_t i = 0; i < t; i++) {
 		puts(e[i]);
 		free(e[i]);
 	}
-	closedir(d);
-	return 0;
+	return closedir(d);
 }
 
-static int
-reveal_ctts(char *path, struct stat *s)
-{
+static int revealmdate(struct stat *s) {
+	char m[29];
+	return strftime(m, sizeof(m), "%a %b %d %T %Z %Y",
+	localtime(&s->st_mtime)) ? puts(m) == EOF : printerr("overflowed "
+	"modified date buffer.", NULL, NULL, NULL);
+}
+
+static int revealctts(char *path, struct stat *s) {
 	switch (s->st_mode & S_IFMT) {
-		RET_CASE(S_IFREG, reveal_file(path));
-		RET_CASE(S_IFDIR, reveal_dir(path));
-		RET_CASE(S_IFLNK, print_err("can't read symlink \"", path,
+		PARSERETCASE(S_IFREG, revealfile(path));
+		PARSERETCASE(S_IFDIR, revealdir(path));
+		PARSERETCASE(S_IFLNK, printerr("can't read symlink \"", path,
 		"\".", "Try to use the \"-fl\" option right before it."));
-	default:
-		return print_err("can't read contents of \"", path, "\".",
-		"Its type is unreadable.");
 	}
+	return printerr("can't read contents of \"", path, "\".", "Its type is "
+	"unreadable.");
 }
 
-static int
-reveal(char *path)
-{
+static int reveal(char *path) {
 	struct stat s;
-	if (FL_LNK ? stat(path, &s) : lstat(path, &s))
-		return print_err("can't find entry \"", path, "\".", "Check "
-		"for misspellings.");
+	if (FLLNK ? stat(path, &s) : lstat(path, &s))
+		return printerr("can't find entry \"", path, "\".", "Check if "
+		"you misspelled it.");
 	switch (DT) {
-		CASE(DT_TYPE, reveal_type(&s));
-		CASE(DT_SIZE, reveal_size(&s));
-		CASE(DT_BT_SIZE, reveal_bt_size(&s));
-		CASE(DT_PERMS, reveal_perms(&s));
-		CASE(DT_OCT_PERMS, reveal_oct_perms(&s));
-		RET_CASE(DT_USR, reveal_usr(&s, path));
-		CASE(DT_USR_ID, reveal_own_id(s.st_uid));
-		RET_CASE(DT_GRP, reveal_grp(&s, path));
-		CASE(DT_GRP_ID, reveal_own_id(s.st_gid));
-		RET_CASE(DT_MOD_DATE, reveal_mod_date(&s));
+		PARSECASE(DT_TYPE, revealtype(&s));
+		PARSECASE(DT_SIZE, revealsize(&s));
+		PARSECASE(DT_BSIZE, revealbsize(&s));
+		PARSECASE(DT_PERMS, revealperms(&s));
+		PARSECASE(DT_OPERMS, revealoperms(&s));
+		PARSERETCASE(DT_USR, revealusr(path, &s));
+		PARSECASE(DT_UID, revealownid(s.st_uid));
+		PARSERETCASE(DT_GRP, revealgrp(path, &s));
+		PARSECASE(DT_GID, revealownid(s.st_gid));
+		PARSERETCASE(DT_MDATE, revealmdate(&s));
 	default:
-		return reveal_ctts(path, &s);
+		return revealctts(path, &s);
 	}
 	return 0;
 }
 
-static int
-parse_lnk_opts(char *path, char *arg, bool is_last_arg)
-{
-	PARSE_LNK_OPT("fl", true);
-	PARSE_LNK_OPT("dfl", false);
+static int parselnkopts(char *path, char *arg, bool islast) {
+	PARSELNKOPT("fl", true);
+	PARSELNKOPT("dfl", false);
 	return 0;
 }
 
-static int
-parse_dt_opts(char *path, char *arg, bool is_last_arg)
-{
-	PARSE_DT_OPT("c", DT_CTTS);
-	PARSE_DT_OPT("t", DT_TYPE);
-	PARSE_DT_OPT("s", DT_SIZE);
-	PARSE_DT_OPT("bs", DT_BT_SIZE);
-	PARSE_DT_OPT("p", DT_PERMS);
-	PARSE_DT_OPT("op", DT_OCT_PERMS);
-	PARSE_DT_OPT("u", DT_USR);
-	PARSE_DT_OPT("ui", DT_USR_ID);
-	PARSE_DT_OPT("g", DT_GRP);
-	PARSE_DT_OPT("gi", DT_GRP_ID);
-	PARSE_DT_OPT("md", DT_MOD_DATE);
+static int parsedtopts(char *path, char *arg, bool islast) {
+	PARSEDTOPT("c", DT_CTTS);
+	PARSEDTOPT("t", DT_TYPE);
+	PARSEDTOPT("s", DT_SIZE);
+	PARSEDTOPT("bs", DT_BSIZE);
+	PARSEDTOPT("p", DT_PERMS);
+	PARSEDTOPT("op", DT_OPERMS);
+	PARSEDTOPT("u", DT_USR);
+	PARSEDTOPT("ui", DT_UID);
+	PARSEDTOPT("g", DT_GRP);
+	PARSEDTOPT("gi", DT_GID);
+	PARSEDTOPT("md", DT_MDATE);
 	return 0;
 }
 
-static void
-parse_non_meta_opts(int tot_args, char **args)
-{
+static void parsenonmetaopts(int totargs, char **args) {
 	char *path = ".";
-	for (int i = 1; i < tot_args; i++) {
+	for (int i = 1; i < totargs; i++) {
 		char *arg = args[i];
-		bool is_last_arg = i == tot_args - 1;
-		if (parse_dt_opts(path, arg, is_last_arg) ||
-		parse_lnk_opts(path, arg, is_last_arg) || reveal(arg))
+		bool islast = i == totargs - 1;
+		if (parsedtopts(path, arg, islast) ||
+		parselnkopts(path, arg, islast) || reveal(arg))
 			continue;
-		AW_ARG = false;
+		AWARG = false;
 		path = arg;
 	}
 }
 
-static void
-parse_meta_opts(int tot_args, char **args)
-{
-	for (int i = 1; i < tot_args; i++) {
-		PARSE_META_OPT("v", puts(PGR_VRS));
-		PARSE_META_OPT("h", help());
+static void parsemetaopts(int totargs, char **args) {
+	for (int i = 1; i < totargs; i++) {
+		PARSEMETAOPT("v", puts(PGRVRS));
+		PARSEMETAOPT("h", help());
 	}
 }
 
-int
-main(int tot_args, char **args)
-{
-	if (tot_args == 1)
-		return EXIT_CODE(reveal("."));
-	parse_meta_opts(tot_args, args);
-	parse_non_meta_opts(tot_args, args);
-	return EXIT_CODE(H_ERR);
+int main(int totargs, char **args) {
+	if (totargs == 1)
+		return PARSEEXITCD(reveal("."));
+	parsemetaopts(totargs, args);
+	parsenonmetaopts(totargs, args);
+	return PARSEEXITCD(HERR);
 }
+
